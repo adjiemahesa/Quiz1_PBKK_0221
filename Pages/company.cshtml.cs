cc846964ef1c4f37ae39ce01c7001b0b23bed564
adjiemahesa <adjiemahesa75@gmail.com>
2022-03-12 14:59:18 +0700

    feat: add navbar to layout + add razor pages

---
+++ Pages/company.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Quiz1_PBKK.Pages
+{
+    public class companyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
